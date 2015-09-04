9f387fb8f472858a7ae134c56b1b245e82b05b4b
Alice Fung <alice.o.fung@gmail.com>
2015-09-04 13:39:37 -0400

    add get and post methods

--- script.js
+++ script.js
@@ -21,8 +21,15 @@
 var $ = {
   ajax: function(options){
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function(){
-      if (this.readyState == 4) {
+    xhr.addEventListener('readystatechange', function(){
+      if(this.readyState ==1){
+        options.open(this);
+      } else if (this.readyState == 2){
+        options.headersDone(this);
+      } else if (this.readyState ==3){
+        options.loading(this);
+      }
+      else if (this.readyState == 4) {
         if (this.status >= 200 && this.status < 300) {
           options.success(this);
         } else {
@@ -31,32 +38,90 @@ var $ = {
         options.complete();
       }
     });
+
     xhr.open(options.method, options.url, options.async);
     if (options.headers) {
       for (key in options.headers) {
         xhr.setRequestHeader(key, options.headers[key]);
       };
     };
+    //readyState ==1
+
     xhr.send(options.data);
+    //readyState ==2
+  },
+
+  get: function(string, function1){
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function1);
+    xhr.open('GET', string, true);
+    xhr.send();
+    return xhr;
+  },
+
+  post: function(url, data, function1){
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function(){
+      if (this.readyState == 4) {
+          function1(this);
+        }
+      });
+    xhr.open('POST', url, true);
+    if (typeof data === 'object'){
+      // var array
+      var string = "";
+      for (key in data){
+        string+= key+ '=' + data[key] + '&';
+      }
+      data = string.slice(0, string.length-1);
+    }
+    xhr.send(data);
+    return xhr;
   }
+
 };
 
-$.ajax({
-  async: true,
-  complete: function () {
-    console.log("complete");
-  },
-  error: function (response) {
-    console.log("an error occurred");
-    console.log(response.status + ": " + response.statusText);
-    console.log(response.responseText);
-  },
-  success: function (response) {
-    console.log("your request succeeded");
-    console.log(response)
-    console.log(response.status)
-    console.log(response.responseText)
-  },
-  method: "GET",
-  url: "http://reqr.es/users/400"
-})
+var xhr3 = $.post("http://reqr.es/api/users/",
+                  {name: 'hello', job: 'greeter'},
+                  function(xhr){
+                    console.log(xhr);
+                  });
+
+
+// var xhr2 = $.get("http://reqr.es/api/users/",
+//                   function(){console.log('logging');});
+
+
+// $.ajax({
+//   async: true,
+//   complete: function () {
+//     console.log("complete");
+//   },
+//   error: function (response) {
+//     console.log("an error occurred");
+//     console.log(response.status + ": " + response.statusText);
+//     console.log(response.responseText);
+//   },
+//   success: function (response) {
+//     console.log("your request succeeded");
+//     console.log(response);
+//     console.log(response.status);
+//     console.log(response.responseText);
+//   },
+//   method: "GET",
+//   url: "http://reqr.es/users/400",
+//   open: function(response){
+//     console.log('open method: ' + response);
+//   },
+//   headersDone: function(response){
+//     console.log('state 2: ' + response);
+//   },
+//   loading: function(response){
+//     console.log('loading: ' + response);
+//   }
+// });
+
+
+
+
+
