c48870516667522100048ef259b7f4ced56c8b6c
David <davidmeza@outlook.com>
2015-09-04 12:45:01 -0400

    add .ajax function

--- script.js
+++ script.js
@@ -20,10 +20,43 @@
 
 var $ = {
   ajax: function(options){
-    //options = {url: ....., xhr : ...}
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function(){
-      console.log('added event listener');
+      if (this.readyState == 4) {
+        if (this.status >= 200 && this.status < 300) {
+          options.success(this);
+        } else {
+          options.error(this);
+        };
+        options.complete();
+      }
     });
+    xhr.open(options.method, options.url, options.async);
+    if (options.headers) {
+      for (key in options.headers) {
+        xhr.setRequestHeader(key, options.headers[key]);
+      };
+    };
+    xhr.send(options.data);
   }
 };
+
+$.ajax({
+  async: true,
+  complete: function () {
+    console.log("complete");
+  },
+  error: function (response) {
+    console.log("an error occurred");
+    console.log(response.status + ": " + response.statusText);
+    console.log(response.responseText);
+  },
+  success: function (response) {
+    console.log("your request succeeded");
+    console.log(response)
+    console.log(response.status)
+    console.log(response.responseText)
+  },
+  method: "GET",
+  url: "http://reqr.es/users/400"
+})
